fdc5286140dcf3a7aa7164d72724d6f6ae1b7105
Smolevich <smolevich90@gmail.com>
2020-01-17 12:33:15 +0300

    Update command

--- app/Console/Commands/TransactionWork.php
+++ app/Console/Commands/TransactionWork.php
@@ -5,6 +5,10 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 use MongoDB\Driver\WriteConcern;
+use MongoDB\Driver\Session;
+use MongoDB\Driver\ReadConcern;
+use MongoDB\Driver\ReadPreference;
+use Throwable;
 
 class TransactionWork extends Command
 {
@@ -15,24 +19,52 @@ class TransactionWork extends Command
     {
         $dbName = 'application';
         $collection = 'transaction_collection';
+        $collectionTwo = 'transaction_collection_2';
 
-        $client = DB::getMongoClient();
-        $client->selectCollection(
-            $dbName,
-            $collection,
-            [
-                'writeConcern' => new WriteConcern(WriteConcern::MAJORITY, 1000),
-            ]
-        )->insertOne(['abc' => 0]);
+        try {
+            $client = DB::getMongoClient();
+            $client->selectCollection(
+                $dbName,
+                $collection,
+                [
+                    'writeConcern' => new WriteConcern(WriteConcern::MAJORITY, 1000),
+                ]
+            )->insertOne(['abc' => 0]);
 
-        $client->selectCollection(
-            $dbName,
-            $collection,
-            [
+            $client->selectCollection(
+                $dbName,
+                $collection,
+                [
+                    'writeConcern' => new WriteConcern(WriteConcern::MAJORITY, 1000),
+                ]
+            )->insertOne(['xyz' => 0]);
+            // Step 1: Define the callback that specifies the sequence of operations to perform inside the transactions.
+
+            $callback = function (Session $session) use ($client, $dbName, $collection, $collectionTwo) {
+                $client
+                    ->selectCollection($dbName, $collection)
+                    ->insertOne(['abc' => 1], ['session' => $session]);
+
+                $client
+                    ->selectCollection($dbName, 'bar')
+                    ->insertOne(['xyz' => 999], ['session' => $session]);
+            };
+
+            // Step 2: Start a client session.
+
+            $session = $client->startSession();
+
+            // Step 3: Use with_transaction to start a transaction, execute the callback, and commit (or abort on error).
+
+            $transactionOptions = [
+                'readConcern' => new ReadConcern(ReadConcern::LOCAL),
                 'writeConcern' => new WriteConcern(WriteConcern::MAJORITY, 1000),
-            ]
-        )->insertOne(['xyz' => 0]);
-        
-        dd($client);
+                'readPreference' => new ReadPreference(ReadPreference::RP_PRIMARY),
+            ];
+
+            \MongoDB\with_transaction($session, $callback, $transactionOptions);
+        }  catch (Throwable $t) {
+            echo sprintf('Error %s in file %s on line %s', $t->getMessage(), $t->getFile(), $t->getLine()).PHP_EOL;
+        }
     }
 }
