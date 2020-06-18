6d4b9605501d1d1803f33ee602258c0c12fb1b17
Smolevich <smolevich90@gmail.com>
2020-06-18 10:33:13 +0300

    Fix error in ci

--- app/Listeners/EloquentListener.php
+++ app/Listeners/EloquentListener.php
@@ -31,10 +31,12 @@ class EloquentListener {
         if ($continue) {
             /** @var \Illuminate\Contracts\Auth\Factory|\Illuminate\Contracts\Auth\Guard|\Illuminate\Contracts\Auth\StatefulGuard $auth */
             $auth = auth();
+            $user = $auth->user();
+
             ActivityLogs::create([
                 'fire_event' => $event,
                 'user_id' => isset($auth) ? $auth->id() : '-1',
-                'site_id' => isset($auth) ? $auth->user()->site_id : '-1'
+                'site_id' => isset($user) ? $user->site_id : '-1'
             ]);
         }
 
